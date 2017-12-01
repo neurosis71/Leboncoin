52aa1c4ed27674750374b6639054992feaf1bbe8
Maxime LEPERS <neurosis71@hotmail.fr>
2017-12-01 12:21:39 +0100

    Creation d'un request filter afin de décoder le json envoyé

---
+++ src/RequestFilters/JsonDecode.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
